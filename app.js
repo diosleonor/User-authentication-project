fbfd8f79294578b710dccda903802fadb19596ef
diosleonor <tccoolovl@gmail.com>
2021-11-14 12:01:41 +0800

    add mongoose

--- app.js
+++ app.js
@@ -4,13 +4,22 @@ const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const app = express()
 const port = 3000
+const mongoose = require('mongoose')
+const Userauthentication = require('./models/userauthentication')
 
-app.use(bodyParser, urlencoded({ extended: true}))
+mongoose.connect('mongodb://localhost/user-authentication')
+const db = mongoose.connection
+db.on('error', () => console.log('Mongoose error!'))
+db.once('open', () => console.log('Mongoose connected!'))
+
+app.use(bodyParser.urlencoded({ extended: true}))
 app.use(methodOverride('_method'))
 
 // route setting
+app.get('/', (req, res) => {
 
+})
 
 //
 
-app.listen(port, () => console.log(`App is running on ${port}`))
+app.listen(port, () => console.log(`App is running on http://localhost:${port}`))
---
+++ models/userauthentication.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const UserauthenticationSchema = new Schema({
+	firstName: {type:String},
+   	email: {type:String},
+   	password: {type:String}
+})
+module.exports = mongoose.model('Userauthentication', UserauthenticationSchema)
